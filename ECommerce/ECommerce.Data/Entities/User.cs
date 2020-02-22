2155ae5691411fed42921b5197d2056dd549f4a1
Can Yaralı <mco2hy@gmail.com>
2020-02-22 19:08:54 +0300

    E-Commerce (22.02.2020)

---
+++ ECommerce/ECommerce.Data/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ECommerce.Data.Entities
+{
+    public class User : Abstracts.Entity
+    {
+        [Required, MaxLength(50), MinLength(2)]
+        public String Name { get; set; }
+
+        [Required, MaxLength(50), MinLength(2)]
+        public String Surname { get; set; }
+
+        [Required, MaxLength(350), MinLength(6)]
+        public String Email { get; set; }
+
+        //Buraya geleceğiz.
+        public String Password { get; set; }
+    }
+}
