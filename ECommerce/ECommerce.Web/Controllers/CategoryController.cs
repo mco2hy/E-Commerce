773c321d2a0982fd2058e541979db9215e04cd90
Mehmet Can Onur Yaralı <mco2hy@gmail.com>
2020-05-02 18:53:11 +0300

    Ok (18:53)

--- ECommerce/ECommerce.Web/Controllers/CategoryController.cs
+++ ECommerce/ECommerce.Web/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using ECommerce.Data.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 
 namespace ECommerce.Web.Controllers
 {
@@ -30,15 +32,24 @@ namespace ECommerce.Web.Controllers
         public IActionResult Save([FromBody] Data.DTO.Category_Save_Request dto)
         {
             if (!ModelState.IsValid) return BadRequest("Kötü çocuk");
+            Data.Entities.Category category = null;
+            Data.Entities.Menu menu = null;
+            Data.Entities.Category parentCategory = null;
 
+            if (dto.MenuId != null)
+            {
+                menu = _unitOfWork.MenuRepository.Get((int)dto.MenuId);
+            }
+
+            if (dto.ParentCategoryId != null)
+            {
+                parentCategory = _unitOfWork.CategoryRepository.Get((int)dto.ParentCategoryId);
+            }
             if (dto.CategoryId == 0)
             {
                 //yeni kayıt
 
-                var menu = _unitOfWork.MenuRepository.Get(dto.MenuId);
-                var parentCategory = _unitOfWork.CategoryRepository.Get(dto.ParentCategoryId);
-
-                var category = new Data.Entities.Category()
+                category = new Data.Entities.Category()
                 {
                     Active = true,
                     CreateDate = DateTime.UtcNow,
@@ -46,14 +57,38 @@ namespace ECommerce.Web.Controllers
                     Name = dto.Name,
                     Parent = parentCategory
                 };
+
+                _unitOfWork.CategoryRepository.Insert(category);
+                _unitOfWork.Complete();
             }
             else
             {
                 //güncelleme
-                var category = _unitOfWork.CategoryRepository.Get(dto.CategoryId);
+                category = _unitOfWork.CategoryRepository
+                    .Query()
+                    .Include(a => a.Parent)
+                    .Include(a => a.Menu)
+                    .SingleOrDefault(a => a.Id == dto.CategoryId);
+
+                if (category == null) return BadRequest("Kaydetmeye çalıştığınız kategori bulunamadı.");
+
+                category.Name = dto.Name;
+                category.Menu = menu;
+                category.Parent = parentCategory;
+
+                _unitOfWork.CategoryRepository.Update(category);
+                _unitOfWork.Complete();
             }
 
             return new JsonResult(category);
+        }
+
+        [Route("/kategori/getir")]
+        public IActionResult GetAll()
+        {
+            var categories = _unitOfWork.CategoryRepository.List();
+
+            return new JsonResult(categories);
         }
     }
 }
--- ECommerce/ECommerce.Web/wwwroot/js/page/Category_Manage.js
+++ ECommerce/ECommerce.Web/wwwroot/js/page/Category_Manage.js
@@ -2,7 +2,7 @@
     CategoryId: 0,
     Init: function (categoryId) {
         Category_Manage.CategoryId = categoryId;
-        Category_Manage.Fetch(categoryId);
+        Category_Manage.Fetch_Menu();
     },
     Fetch: function (categoryId) {
         if (categoryId == 0) return;
@@ -19,6 +19,34 @@
     Fetch_Callback: function (result) {
         console.log(result);
         $("#category-manage-catagoryname").val(result.name);
+    },
+    Fetch_Menu: function () {
+        $.ajax({
+            type: "GET",
+            url: "/menu/getir/",
+            data: [],
+            dataType: "json",
+            contentType: "application/json; charset=utf-8;",
+            success: Category_Manage.Fetch_Menu_Callback
+        });
+    },
+    Fetch_Menu_Callback: function (result) {
+        console.log(result);
+        Category_Manage.Fetch_Category();
+    },
+    Fetch_Category: function () {
+        $.ajax({
+            type: "GET",
+            url: "/kategori/getir/",
+            data: [],
+            dataType: "json",
+            contentType: "application/json; charset=utf-8;",
+            success: Category_Manage.Fetch_Category_Callback
+        });
+    },
+    Fetch_Category_Callback: function (result) {
+        console.log(result);
+        Category_Manage.Fetch(Category_Manage.categoryId);
     },
     Save: function () {
         var name = $("#category-manage-categoryname").val();
